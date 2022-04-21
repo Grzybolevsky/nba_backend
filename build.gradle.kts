2dfbaef3c213dfeadca972268c1bd4064faea614
Szymon Żak <46254781+Grzybolevsky@users.noreply.github.com>
2022-04-22 01:26:31 +0200

    Added Spring Security. Added integration test module. (#9)
    * Added Spring Security. Added integration test module.
    
    * Upgraded Gradle and plugins. Added auto-linter.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("java")
     id("application")
-    id("org.springframework.boot") version "2.6.6"
+    id("jvm-test-suite")
+    id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.freefair.lombok") version "6.4.1"
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
-    kotlin("plugin.jpa") version "1.6.10"
+    id("io.freefair.lombok") version "6.4.3"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 configurations {
     compileOnly {
@@ -28,19 +32,21 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    runtimeOnly("org.postgresql:postgresql:42.3.3")
-    runtimeOnly("com.h2database:h2:2.1.210")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2:2.1.212")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
@@ -56,4 +62,42 @@ tasks.withType<Test> {
 
 application {
     mainClass.set("com.example.ApplicationKt")
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+
+        val integration by registering(JvmTestSuite::class) {
+            dependencies {
+                implementation(project)
+            }
+
+            targets {
+                all {
+                    testTask.configure {
+                        shouldRunAfter(test)
+                    }
+                }
+            }
+        }
+    }
+}
+
+val integrationImplementation by configurations.getting {
+    extendsFrom(configurations.testImplementation.get())
+}
+
+tasks.check {
+    dependsOn(testing.suites.named("integration"))
+}
+
+tasks.compileKotlin {
+    dependsOn(tasks.named("ktlintFormat"))
+}
+
+tasks.clean {
+    dependsOn(tasks.named("ktlintFormat"))
 }
---
+++ src/integration/kotlin/com/example/IntegrationTests.kt
@@ -0,0 +1,24 @@
+package com.example
+
+import com.example.config.IntegrationTestConfiguration
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(IntegrationTestConfiguration::class)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @Test
+    fun `Assert blog page title, content and status code`() {
+        println(">> Assert blog page title, content and status code")
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Blog</h1>")
+    }
+}
