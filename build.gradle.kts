6d3cdf464bbe114ed89b57dd36ab83fdcecc4646
Szymon Żak <szymon436@gmail.com>
2022-04-22 01:22:45 +0200

    Upgraded Gradle and plugins. Added auto-linter.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("application")
-    id("org.springframework.boot") version "2.6.6"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.freefair.lombok") version "6.4.1"
     id("java")
+    id("application")
     id("jvm-test-suite")
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
-    kotlin("plugin.jpa") version "1.6.10"
+    id("org.springframework.boot") version "2.6.7"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("io.freefair.lombok") version "6.4.3"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 configurations {
     compileOnly {
@@ -34,15 +36,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
-    implementation("junit:junit:4.13.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    runtimeOnly("org.postgresql:postgresql:42.3.3")
-    runtimeOnly("com.h2database:h2:2.1.210")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2:2.1.212")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
@@ -89,6 +90,14 @@ val integrationImplementation by configurations.getting {
     extendsFrom(configurations.testImplementation.get())
 }
 
-tasks.named("check") {
+tasks.check {
     dependsOn(testing.suites.named("integration"))
+}
+
+tasks.compileKotlin {
+    dependsOn(tasks.named("ktlintFormat"))
+}
+
+tasks.clean {
+    dependsOn(tasks.named("ktlintFormat"))
 }
