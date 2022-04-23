e4b4dc7f5facb41b4022f328b42d333558057c57
Szymon Żak <szymon436@gmail.com>
2022-04-23 11:29:46 +0200

    Test fixup.

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -24,22 +24,21 @@ fun getDataFromBalldontile(urlString: String): JSONArray {
             dataString += line
         }
     }
-    var rawJSON = JSONObject(dataString)
-    var arrayFromJSON: JSONArray = rawJSON.getJSONArray("data")
-    return arrayFromJSON
+    val rawJSON = JSONObject(dataString)
+    return rawJSON.getJSONArray("data")
 }
 
 fun main(args: Array<String>) {
-    val playersURL: String = "https://www.balldontlie.io/api/v1/players"
-    var playersData: JSONArray = getDataFromBalldontile(playersURL)
+    val playersURL = "https://www.balldontlie.io/api/v1/players"
+    val playersData: JSONArray = getDataFromBalldontile(playersURL)
 
-    var gamesURL: String = "https://www.balldontlie.io/api/v1/games"
-    var gamesData: JSONArray = getDataFromBalldontile(gamesURL)
+    val gamesURL = "https://www.balldontlie.io/api/v1/games"
+    val gamesData: JSONArray = getDataFromBalldontile(gamesURL)
 
-    var players: ArrayList<Player> = ArrayList<Player>()
+    val players: ArrayList<Player> = ArrayList()
     for (i in 0 until playersData.length()) {
-        var tempPlayerJSON: JSONObject = playersData.getJSONObject(i)
-        var tempPlayer: Player = Player(
+        val tempPlayerJSON: JSONObject = playersData.getJSONObject(i)
+        val tempPlayer = Player(
             tempPlayerJSON["id"] as Int,
             tempPlayerJSON["first_name"] as String,
             tempPlayerJSON["last_name"] as String,
@@ -48,13 +47,13 @@ fun main(args: Array<String>) {
         players.add(tempPlayer)
     }
 
-    var player = players.get(0)
+    val player = players[0]
     println("===============================")
     println("EXAMPLE FIRST PLAYER: ")
     println(player.firstName + " " + player.lastName + " " + player.position)
     println("===============================")
 
-    var game = gamesData.get(0)
+    val game = gamesData[0]
     println("===============================")
     println("EXAMPLE FIRST GAME: ")
     println(game)
