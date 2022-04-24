bd1c91075b151d8da8675c75d995cea3b81a47ab
Szymon Żak <szymon436@gmail.com>
2022-04-24 21:29:39 +0200

    Change framework to Ktor.

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,44 @@
+package com.example.plugins
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.locations.Location
+import io.ktor.server.locations.Locations
+import io.ktor.server.locations.get
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun Application.configureRouting() {
+    install(Locations) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get<MyLocation> {
+            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+        }
+        // Register nested routes
+        get<Type.Edit> {
+            call.respondText("Inside $it")
+        }
+        get<Type.List> {
+            call.respondText("Inside $it")
+        }
+    }
+}
+
+@Location("/location/{name}")
+class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+
+@Location("/type/{name}")
+data class Type(val name: String) {
+    @Location("/edit")
+    data class Edit(val type: Type)
+
+    @Location("/list/{page}")
+    data class List(val type: Type, val page: Int)
+}
