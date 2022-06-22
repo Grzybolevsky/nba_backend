cf52eabbd8b1a840cdef02907f573dc4a9a81a65
Szymon Żak <szymon436@gmail.com>
2022-06-22 05:03:51 +0200

    Fixup: session_id to user_id

--- src/main/kotlin/com/example/services/FavoriteService.kt
+++ src/main/kotlin/com/example/services/FavoriteService.kt
@@ -3,29 +3,40 @@ package com.example.services
 import com.example.dao.DAOFacadeFavorites
 import com.example.model.FavoritePlayer
 import com.example.model.FavoriteTeam
+import com.example.security.UserInfo
 
 object FavoriteService {
-    fun getFavoritePlayers(userId: Int): List<FavoritePlayer> {
-        return DAOFacadeFavorites.getFavoritePlayers(userId)
+    private val userInfoService = UserInfoService
+
+    fun getFavoritePlayers(sessionId: String): List<FavoritePlayer> {
+        val userInfo: UserInfo = userInfoService.getUserInfo(sessionId)
+
+        return DAOFacadeFavorites.getFavoritePlayers(userInfo.id.hashCode())
     }
 
-    fun deleteFavoritePlayer(userId: Int, playerId: Int): Boolean {
-        return DAOFacadeFavorites.deleteFavoritePlayer(userId, playerId)
+    fun deleteFavoritePlayer(sessionId: String, playerId: Int): Boolean {
+        val userInfo: UserInfo = userInfoService.getUserInfo(sessionId)
+
+        return DAOFacadeFavorites.deleteFavoritePlayer(userInfo.id.hashCode(), playerId)
     }
 
-    fun addFavoritePlayer(userId: Int, playerId: Int) {
-        DAOFacadeFavorites.addFavoritePlayer(userId, playerId)
+    fun addFavoritePlayer(sessionId: String, playerId: Int) {
+        val userInfo: UserInfo = userInfoService.getUserInfo(sessionId)
+        DAOFacadeFavorites.addFavoritePlayer(userInfo.id.hashCode(), playerId)
     }
 
-    fun getFavoriteTeams(userId: Int): List<FavoriteTeam> {
-        return DAOFacadeFavorites.getFavoriteTeams(userId)
+    fun getFavoriteTeams(sessionId: String): List<FavoriteTeam> {
+        val userInfo: UserInfo = userInfoService.getUserInfo(sessionId)
+        return DAOFacadeFavorites.getFavoriteTeams(userInfo.id.hashCode())
     }
 
-    fun addFavoriteTeam(userId: Int, teamId: Int) {
-        DAOFacadeFavorites.addFavoriteTeam(userId, teamId)
+    fun addFavoriteTeam(sessionId: String, teamId: Int) {
+        val userInfo: UserInfo = userInfoService.getUserInfo(sessionId)
+        DAOFacadeFavorites.addFavoriteTeam(userInfo.id.hashCode(), teamId)
     }
 
-    fun deleteFavoriteTeam(userId: Int, teamId: Int): Boolean {
-        return DAOFacadeFavorites.deleteFavoriteTeam(userId, teamId)
+    fun deleteFavoriteTeam(sessionId: String, teamId: Int): Boolean {
+        val userInfo: UserInfo = userInfoService.getUserInfo(sessionId)
+        return DAOFacadeFavorites.deleteFavoriteTeam(userInfo.id.hashCode(), teamId)
     }
 }
